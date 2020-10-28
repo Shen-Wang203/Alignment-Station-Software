c2231cf647ce553387e3f1a6472833b35018b0f7
Shen Wang <swang@diconfiberoptics.com>
2020-10-28 13:30:05 -0700

    Beetle Math Model Class

--- Console_for_tests/Console_for_tests/Program.cs
+++ Console_for_tests/Console_for_tests/Program.cs
@@ -6,7 +6,13 @@ namespace Console_for_tests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BeetleMathModel.set_pivotPoint = new float[] { 0, -2.1f, 5.62f, 0 };
+            double[] L = BeetleMathModel.FindAxialPosition(0, 0.6f, 144, 0.2f, -2, 0);
+            foreach (float i in BeetleMathModel.set_pivotPoint)
+                Console.WriteLine(i);
+            foreach (double i in L)
+                Console.WriteLine(i);
+
         }
     }
 }
